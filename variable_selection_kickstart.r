7495d509cac8e2e9850fa726cc1727033817f1d7
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2023-09-20 20:54:21 -0300

    Update variable_selection_kickstart.r

--- variable_selection_kickstart.r
+++ variable_selection_kickstart.r
@@ -40,3 +40,51 @@ terror_selected$date <- as.Date(terror_selected$date)
 sum(is.na(terror_selected$date))#no NAs
 
 #preprocessed data
+
+#loading more data
+
+df_terrorism2021 <- read_xlsx("C:\\Users\\lucas\\OneDrive\\Área de Trabalho\\TrabalhoDM\\globalterrorismdb_2021Jan-June_1222dist.xlsx")
+
+
+#filter only labels that are terrorist attack (no doubt)
+
+terrorism <- df_terrorism2021 %>% filter(doubtterr!=1)#remove those with doubt
+terrorism <- terrorism %>% filter(region_txt=="South America")#Keep South America
+
+#variable selection
+
+terror_selected2 <- terrorism %>% select(iyear,imonth,iday,country_txt,provstate,city,success)
+terror_selected2 <- terror_selected2 %>% filter(iday>0)#filtering unknown dates
+terror_selected2 <- terror_selected2 %>% filter(provstate!="Unknown")#filtering unknown provinvies
+terror_selected2 <- terror_selected2 %>% filter(city!="Unknown")#filtering unknown cities
+
+#looking for NAs
+sum(is.na(terror_selected2$city))
+sum(is.na(terror_selected2$provstate))
+#
+table(terror_selected2$success)/10000#12,67% in the target class
+
+#padding string to the size of 2 (adding 0 to the left of size 1)
+terror_selected2$iday <- str_pad(terror_selected2$iday,2, pad = "0")
+terror_selected2$imonth <- str_pad(terror_selected2$imonth,2, pad = "0")
+
+terror_selected2$date <- paste(terror_selected2$iyear,"-",terror_selected2$imonth,"-",terror_selected2$iday,sep="")
+
+terror_selected2$date <- as.Date(terror_selected2$date)
+
+sum(is.na(terror_selected2$date))#no NAs
+
+###join data
+
+terror_selected <- rbind(terror_selected,terror_selected2)
+
+terror_selected_target <- terror_selected %>% filter(success==0)
+terror_selected_comp <- terror_selected %>% filter(success==1)
+
+#setting a seed for random sampling
+set.seed(3)#run before sampling
+terror_selected_comp <- sample_n(terror_selected_comp, 10000-nrow(terror_selected_target))
+
+terror_selected <- rbind(terror_selected_comp,terror_selected_target)
+
+table(terror_selected$success)/nrow(terror_selected)#87,21% of success rate
