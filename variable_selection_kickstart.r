45db98edd5160008c008211c8d67c5f87b9a3cb0
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2023-09-21 09:55:59 -0300

    Update variable_selection_kickstart.r

--- variable_selection_kickstart.r
+++ variable_selection_kickstart.r
@@ -80,17 +80,47 @@ terror_selected <- rbind(terror_selected,terror_selected2)
 
 terror_selected_target <- terror_selected %>% filter(success==0)
 terror_selected_comp <- terror_selected %>% filter(success==1)
+terror_selected_comp <- terror_selected_comp %>% filter(country_txt!="Falkland Islands")
 
 #setting a seed for random sampling
-set.seed(3)#run before sampling
+set.seed(2)#run before sampling
 terror_selected_comp <- sample_n(terror_selected_comp, 10000-nrow(terror_selected_target))
+
 
 terror_selected <- rbind(terror_selected_comp,terror_selected_target)
 
 table(terror_selected$success)/nrow(terror_selected)#87,20% of success rate
+table(terror_selected$country_txt)
 
 #KDD and Data Mining
 
-weekdays(terror_selected$date)#change to english - https://stackoverflow.com/questions/17031002/get-weekdays-in-english-in-r
-week(terror_selected$date)#how many weeks passed
+terror_selected$week_of_year <- week(terror_selected$date)#how many weeks passed
 #is it holiday https://pypi.org/project/holidays/
+#terror_selected$weekday <-weekdays(terror_selected$date)#change to english - https://stackoverflow.com/questions/17031002/get-weekdays-in-english-in-r
+
+df <- terror_selected %>% filter(success==1) %>% group_by(week_of_year) %>% summarise(total=n())
+
+cor(df$week_of_year,df$total,method = "spearman")
+
+plot(df$week_of_year,df$total)
+
+###
+library(WDI)     # for World Bank goodness
+library(GetBCBData)
+library(OECD)
+
+#world bank data
+unique(terror_selected$country_txt)
+?WDI
+
+bmundial<- WDI(
+  country = c("BR","BOL"),
+  #indicator = c("SP.RUR.TOTL","SP.URB.TOTL","SP.POP.TOTL","FP.CPI.TOTL.ZG","NE.TRD.GNFS.ZS","EN.ATM.CO2E.KT"),
+  start = 1970,
+  end = 2021,
+  extra = TRUE,
+  cache = NULL,
+  latest = NULL,
+  language = "en"
+)
+
